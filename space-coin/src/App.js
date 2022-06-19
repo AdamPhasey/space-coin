6c8954e53f63a6b1c0eb4c8fcbaa69527196c62d
Adam P <adam.phasey@hotmail.com>
2022-06-19 11:23:20 +0100

    Added logo

---
+++ space-coin/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <img src={require ('./design-files/logo.png')} className='logo1' alt="logo1" />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
